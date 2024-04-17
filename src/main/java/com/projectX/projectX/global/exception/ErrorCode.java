f1d81edd2daac7c96547073029db1549a22681fd
Yurim Yang <yang980329@likelion.org>
2024-04-17 19:17:42 +0900

    feat: 회원가입 exception 생성
    - AlreadyExistIdException : 중복 id
    - InvalidPasswordException : pw == id

--- src/main/java/com/projectX/projectX/global/exception/ErrorCode.java
+++ src/main/java/com/projectX/projectX/global/exception/ErrorCode.java
@@ -5,6 +5,10 @@ import org.springframework.http.HttpStatus;
 
 @Getter
 public enum ErrorCode {
+
+    // MEMBER
+    ALREADY_EXIST_ID_EXCEPTION(HttpStatus.BAD_REQUEST, "이미 사용중인 아이디입니다."),
+    INVALID_PASSWORD_EXCEPTION(HttpStatus.BAD_REQUEST, "비밀번호는 아이디와 같을 수 없습니다.")
     ; //Error Code를 작성한 마지막에 ;를 추가합니다.
 
     private final HttpStatus httpStatus;
