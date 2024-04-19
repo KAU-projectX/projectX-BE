4607a6718c54f4cf2afb39be3b9f2ff98bb7e44c
jun23314 <dlwnsfml@naver.com>
2024-04-20 01:37:58 +0900

    fix: repository 이름 변경

--- src/main/java/com/projectX/projectX/domain/tour/repository/SidoRepository.java
+++ src/main/java/com/projectX/projectX/domain/tour/repository/SidoRepository.java
@@ -3,6 +3,6 @@ package com.projectX.projectX.domain.tour.repository;
 import com.projectX.projectX.domain.tour.entity.Sido;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface TourRepository extends JpaRepository<Sido, Long> {
+public interface SidoRepository extends JpaRepository<Sido, Long> {
 
 }
