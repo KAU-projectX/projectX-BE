9f84c308004ec691aa9824dcfbfd9228ac1d0778
jun23314 <dlwnsfml@naver.com>
2024-04-26 12:01:22 +0900

    fix: Exception 코드 수정

--- src/main/java/com/projectX/projectX/domain/tour/exception/InvalidAreaCodeException.java
+++ src/main/java/com/projectX/projectX/domain/tour/exception/InvalidAreaCodeException.java
@@ -3,7 +3,7 @@ package com.projectX.projectX.domain.tour.exception;
 import com.projectX.projectX.global.exception.ApplicationException;
 import com.projectX.projectX.global.exception.ErrorCode;
 
-public class InvalidAreaCodeException extends ApplicationException{
+public class InvalidAreaCodeException extends ApplicationException {
 
     public InvalidAreaCodeException(ErrorCode errorCode) {
         super(errorCode);
