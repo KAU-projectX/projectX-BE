f9c30aa00b974f0753652620c7f6768a5af54bd5
jun23314 <dlwnsfml@naver.com>
2024-05-30 23:58:14 +0900

    add: Error Code 추가 - INVALID_PROTOCOL_EXCEPTION - UNSUPPORTED_ENCODING_EXCEPTION - CANNOT_PARSE_JSON_EXCEPTION

--- src/main/java/com/projectX/projectX/global/exception/ErrorCode.java
+++ src/main/java/com/projectX/projectX/global/exception/ErrorCode.java
@@ -15,7 +15,11 @@ public enum ErrorCode {
     INVALID_REQUEST_EXCEPTION(HttpStatus.BAD_REQUEST, "유효하지 않은 API 요청입니다."),
     CONTENT_ID_NOT_FOUND_EXCEPTION(HttpStatus.NOT_FOUND, "존재하지 않은 콘텐트 ID입니다."),
     JEJU_REGION_NOT_FOUND(HttpStatus.BAD_REQUEST,
-        "제주 지역을 찾을 수 없습니다.");//Error Code를 작성한 마지막에 ;를 추가합니다.
+        "제주 지역을 찾을 수 없습니다."),
+    INVALID_PROTOCOL_EXCEPTION(HttpStatus.BAD_REQUEST, "유효하지 않은 프로토콜 요청입니다."),
+    UNSUPPORTED_ENCODING_EXCEPTION(HttpStatus.UNSUPPORTED_MEDIA_TYPE, "지원하지 않는 인코딩입니다."),
+    CANNOT_PARSE_JSON_EXCEPTION(HttpStatus.NOT_ACCEPTABLE,
+        "json을 parse할 수 없습니다.");//Error Code를 작성한 마지막에 ;를 추가합니다.
 
 
     private final HttpStatus httpStatus;
