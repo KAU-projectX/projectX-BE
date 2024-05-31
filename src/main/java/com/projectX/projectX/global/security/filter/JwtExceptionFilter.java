aa514bf6e733f69b0d2f60f55264041edf3fbc0d
Yurim Yang <yang980329@likelion.org>
2024-05-31 18:13:46 +0900

    feat: 인가를 위한 jwt token 로직 구현
    - util, filter를 생성하여 jwt 로직 구현

---
+++ src/main/java/com/projectX/projectX/global/security/filter/JwtExceptionFilter.java
@@ -0,0 +1,30 @@
+package com.projectX.projectX.global.security.filter;
+
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+@Component
+@RequiredArgsConstructor
+public class JwtExceptionFilter extends OncePerRequestFilter {
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response,
+        FilterChain filterChain) throws ServletException, IOException {
+        try {
+            filterChain.doFilter(request, response);
+        } catch (Exception e){
+            response.setStatus(HttpStatus.FORBIDDEN.value());
+            response.setContentType(MediaType.APPLICATION_JSON_VALUE);
+            response.setCharacterEncoding("UTF-8");
+            response.getWriter().write("JWT Token에 문제가 발생했습니다.");
+        }
+    }
+}
