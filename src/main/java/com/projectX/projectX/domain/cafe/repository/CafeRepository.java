812488b52e12c96834b4a1f7b6210442ed3e8a34
jun23314 <dlwnsfml@naver.com>
2024-06-01 16:32:05 +0900

    add: cafeId로 찾는 함수 추가

--- src/main/java/com/projectX/projectX/domain/cafe/repository/CafeRepository.java
+++ src/main/java/com/projectX/projectX/domain/cafe/repository/CafeRepository.java
@@ -7,4 +7,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 public interface CafeRepository extends JpaRepository<Cafe, Long> {
 
     Optional<Cafe> findByNameAndAddress(String name, String Address);
+    Optional<Cafe> findByCafeId(String cafeId);
 }
