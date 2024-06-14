0d2a03aced5696a12fd2e5c47258e6ccf8656e3b
Yurim Yang <yang980329@likelion.org>
2024-06-15 01:49:30 +0900

    fix: woravel swagger 변경

--- src/main/java/com/projectX/projectX/global/config/SwaggerConfig.java
+++ src/main/java/com/projectX/projectX/global/config/SwaggerConfig.java
@@ -1,20 +1,36 @@
 package com.projectX.projectX.global.config;
 
+import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
+import io.swagger.v3.oas.models.security.SecurityScheme.In;
+import io.swagger.v3.oas.models.security.SecurityScheme.Type;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class SwaggerConfig {
+
     @Bean
-    public OpenAPI openAPI(){
+    public OpenAPI openAPI() {
         Info info = new Info()
             .version("v1.0.0")
-            .title("Info For U")
-            .description("Info For U Swagger 입니다");
+            .title("WORAVEL")
+            .description("WORAVEL Swagger 입니다");
+
+        String jwtSchemeName = "Authorization";
+        SecurityRequirement securityRequirement = new SecurityRequirement().addList(jwtSchemeName);
+        Components components = new Components()
+            .addSecuritySchemes(jwtSchemeName, new SecurityScheme()
+                .name(jwtSchemeName)
+                .type(Type.APIKEY)
+                .in(In.HEADER));
 
         return new OpenAPI()
-            .info(info);
+            .info(info)
+            .addSecurityItem(securityRequirement)
+            .components(components);
     }
 }
