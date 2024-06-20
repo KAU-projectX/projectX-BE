b59bfea5b26151501317971fadf25a6d82209f27
jun23314 <dlwnsfml@naver.com>
2024-06-20 23:11:58 +0900

    feat: work 상세 정보 조회

---
+++ src/main/java/com/projectX/projectX/domain/work/util/WorkMapper.java
@@ -0,0 +1,35 @@
+package com.projectX.projectX.domain.work.util;
+
+import com.projectX.projectX.domain.cafe.entity.Cafe;
+import com.projectX.projectX.domain.work.dto.response.WorkGetAllResponse;
+import com.projectX.projectX.domain.work.dto.response.WorkGetDetailResponse;
+import lombok.AccessLevel;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor(access = AccessLevel.PRIVATE)
+public class WorkMapper {
+
+    public static WorkGetAllResponse toWorkGetAllResponse(Cafe cafe) {
+        return WorkGetAllResponse.builder()
+            .id(cafe.getId())
+            .name(cafe.getName())
+            .address(cafe.getAddress())
+            .phone(cafe.getPhoneNumber())
+            .build();
+    }
+
+    public static WorkGetDetailResponse toWorkGetDetailResponse(Cafe cafe) {
+        return WorkGetDetailResponse.builder()
+            .id(cafe.getId())
+            .address(cafe.getAddress())
+            .name(cafe.getName())
+            .uri(cafe.getUri())
+            .phone(cafe.getPhoneNumber())
+            .latitude(cafe.getLatitude())
+            .longitude(cafe.getLongitude())
+            .cafeType(cafe.getCafeType())
+            .jejuRegion(cafe.getJejuRegion())
+            .build();
+    }
+
+}
