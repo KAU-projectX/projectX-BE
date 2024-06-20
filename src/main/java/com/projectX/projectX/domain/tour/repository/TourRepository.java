f24a0430b9435215537ae93bb4979fb26ba77e1e
Yurim Yang <yang980329@likelion.org>
2024-06-20 10:40:32 +0900

    feat: 트래블 전체 조회시, 필요한 repo 구현

--- src/main/java/com/projectX/projectX/domain/tour/repository/TourRepository.java
+++ src/main/java/com/projectX/projectX/domain/tour/repository/TourRepository.java
@@ -1,10 +1,19 @@
 package com.projectX.projectX.domain.tour.repository;
 
 import com.projectX.projectX.domain.tour.entity.Tour;
+import com.projectX.projectX.global.common.ContentType;
+import com.projectX.projectX.global.common.JejuRegion;
 import java.util.Optional;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface TourRepository extends JpaRepository<Tour, Long> {
 
     Optional<Tour> findByContentId(Long contentId);
+
+    Page<Tour> findByContentType(ContentType contentType, Pageable pageable);
+
+    Page<Tour> findByContentTypeAndJejuRegion(ContentType contentType, JejuRegion jejuRegion,
+        Pageable pageable);
 }
