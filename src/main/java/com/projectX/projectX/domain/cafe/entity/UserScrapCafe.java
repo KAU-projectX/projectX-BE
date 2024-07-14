2b82f71ce60637e2ca781db598213930e31cc19d
jun23314 <dlwnsfml@naver.com>
2024-07-14 14:17:37 +0900

    fix: fetchtype eager로 변경

--- src/main/java/com/projectX/projectX/domain/cafe/entity/UserScrapCafe.java
+++ src/main/java/com/projectX/projectX/domain/cafe/entity/UserScrapCafe.java
@@ -1,29 +1,38 @@
 package com.projectX.projectX.domain.cafe.entity;
 
 import com.projectX.projectX.domain.member.entity.Member;
-import jakarta.persistence.Embeddable;
+import jakarta.persistence.Entity;
 import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
-import java.io.Serializable;
-import lombok.EqualsAndHashCode;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
 import lombok.NoArgsConstructor;
 
-@Embeddable
-@NoArgsConstructor
-@EqualsAndHashCode
-public class UserScrapCafe implements Serializable {
-    @ManyToOne(fetch = FetchType.LAZY)
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class UserScrapCafe {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.EAGER)
     @JoinColumn(name = "cafe_id")
     private Cafe cafe;
 
-    @ManyToOne(fetch = FetchType.LAZY)
+    @ManyToOne(fetch = FetchType.EAGER)
     @JoinColumn(name = "member_id")
     private Member member;
 
-    public UserScrapCafe(Cafe cafe, Member member){
+    @Builder
+    public UserScrapCafe(Cafe cafe, Member member) {
         this.cafe = cafe;
         this.member = member;
     }
-
 }
