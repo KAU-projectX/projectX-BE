c6968fca264d36c3bb42272238f867f29f69d9ca
jun23314 <dlwnsfml@naver.com>
2024-07-28 15:09:57 +0900

    add: mapping 정보 추가 - TourReviewImage

--- src/main/java/com/projectX/projectX/domain/review/entity/TourReview.java
+++ src/main/java/com/projectX/projectX/domain/review/entity/TourReview.java
@@ -3,12 +3,16 @@ package com.projectX.projectX.domain.review.entity;
 import com.projectX.projectX.domain.member.entity.Member;
 import com.projectX.projectX.domain.tour.entity.Tour;
 import com.projectX.projectX.global.common.BaseEntity;
+import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToMany;
+import java.util.ArrayList;
+import java.util.List;
 import lombok.AccessLevel;
 import lombok.Builder;
 import lombok.Getter;
@@ -40,6 +44,10 @@ public class TourReview extends BaseEntity {
     @Comment("투어 id")
     private Tour tour;
 
+    @Comment("tour 리뷰 이미지")
+    @OneToMany(mappedBy = "TourReview", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<TourReviewImage> tourReviewImages;
+
     @Builder
     public TourReview(Long id, String contents, Float score, Member user, Tour tour) {
         this.id = id;
@@ -47,5 +55,6 @@ public class TourReview extends BaseEntity {
         this.score = score;
         this.user = user;
         this.tour = tour;
+        this.tourReviewImages = new ArrayList<>();
     }
 }
