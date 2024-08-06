54ae5e6e76daaa20a59d5b9d0cc9e899dfe5f339
jun23314 <dlwnsfml@naver.com>
2024-08-06 10:29:06 +0900

    refactor: 추천 api 파라미터 변경 - JejuRegion에서 String으로 변경

--- src/main/java/com/projectX/projectX/domain/travel/controller/TravelRestController.java
+++ src/main/java/com/projectX/projectX/domain/travel/controller/TravelRestController.java
@@ -3,10 +3,10 @@ package com.projectX.projectX.domain.travel.controller;
 import com.projectX.projectX.domain.travel.dto.response.TravelGetAllResponse;
 import com.projectX.projectX.domain.travel.dto.response.TravelGetSpecificResponse;
 import com.projectX.projectX.domain.travel.service.TravelService;
-import com.projectX.projectX.global.common.JejuRegion;
 import com.projectX.projectX.global.common.ResponseDTO;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
+import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import java.util.List;
 import lombok.RequiredArgsConstructor;
@@ -53,7 +53,7 @@ public class TravelRestController {
     @ResponseStatus(HttpStatus.OK)
     @Operation(summary = "travel 추천 API", description = "travel 추천하는 API입니다.")
     public ResponseDTO<?> getRecommdTravelInfo(
-        @RequestParam JejuRegion jejuRegion
+        @RequestParam @NotBlank String jejuRegion
     ) {
         return ResponseDTO.res(travelService.getTravelRecommd(jejuRegion), "travel 추천 조회에 성공했습니다.");
     }
