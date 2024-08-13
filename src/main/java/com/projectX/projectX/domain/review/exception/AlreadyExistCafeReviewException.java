97d97ff66490685d4aff111cc9964b6c76882636
jun23314 <dlwnsfml@naver.com>
2024-08-13 11:59:06 +0900

    chore: exception 파일 명 변경 - Exception 추가

--- src/main/java/com/projectX/projectX/domain/review/exception/AlreadyExistCafeReviewException.java
+++ src/main/java/com/projectX/projectX/domain/review/exception/AlreadyExistCafeReviewException.java
@@ -3,9 +3,9 @@ package com.projectX.projectX.domain.review.exception;
 import com.projectX.projectX.global.exception.ApplicationException;
 import com.projectX.projectX.global.exception.ErrorCode;
 
-public class AlreadyExistCafeReview extends ApplicationException {
+public class AlreadyExistCafeReviewException extends ApplicationException {
 
-    public AlreadyExistCafeReview(ErrorCode errorCode) {
+    public AlreadyExistCafeReviewException(ErrorCode errorCode) {
         super(errorCode);
     }
 }
