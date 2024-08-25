d71b1ad95d752154fba141f4eab601383227231f
jun23314 <dlwnsfml@naver.com>
2024-08-25 23:16:22 +0900

    add: 카페 리뷰 repository 함수 추가 - cafe로 cafe review 찾기

--- src/main/java/com/projectX/projectX/domain/review/repository/CafeReviewRepository.java
+++ src/main/java/com/projectX/projectX/domain/review/repository/CafeReviewRepository.java
@@ -1,10 +1,14 @@
 package com.projectX.projectX.domain.review.repository;
 
+import com.projectX.projectX.domain.cafe.entity.Cafe;
 import com.projectX.projectX.domain.member.entity.Member;
 import com.projectX.projectX.domain.review.entity.CafeReview;
+import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface CafeReviewRepository extends JpaRepository<CafeReview, Long> {
 
     Boolean existsByUser(Member member);
+
+    Optional<CafeReview> findByCafe(Cafe cafe);
 }
