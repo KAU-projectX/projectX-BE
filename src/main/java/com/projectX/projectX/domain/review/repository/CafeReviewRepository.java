2cb9a6fa04b7b7ec4a3b6de483b9938fe3a096b5
jun23314 <dlwnsfml@naver.com>
2024-08-27 22:03:26 +0900

    add: 리뷰 조회에 필요한 method 생성 - findByCafe

--- src/main/java/com/projectX/projectX/domain/review/repository/CafeReviewRepository.java
+++ src/main/java/com/projectX/projectX/domain/review/repository/CafeReviewRepository.java
@@ -1,10 +1,15 @@
 package com.projectX.projectX.domain.review.repository;
 
+import com.projectX.projectX.domain.cafe.entity.Cafe;
 import com.projectX.projectX.domain.member.entity.Member;
 import com.projectX.projectX.domain.review.entity.CafeReview;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface CafeReviewRepository extends JpaRepository<CafeReview, Long> {
 
     Boolean existsByUser(Member member);
+
+    Page<CafeReview> findByCafe(Cafe cafe, Pageable pageable);
 }
