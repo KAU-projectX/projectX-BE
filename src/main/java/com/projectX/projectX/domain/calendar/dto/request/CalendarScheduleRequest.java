2d129bccf52279d4150c87eb373b7bbd20e46488
Yurim Yang <yang980329@likelion.org>
2024-09-04 22:09:17 +0900

    fix: LocalDate NotEmpty 불가로 인한 NotNull 변경

--- src/main/java/com/projectX/projectX/domain/calendar/dto/request/CalendarScheduleRequest.java
+++ src/main/java/com/projectX/projectX/domain/calendar/dto/request/CalendarScheduleRequest.java
@@ -2,15 +2,16 @@ package com.projectX.projectX.domain.calendar.dto.request;
 
 import com.projectX.projectX.domain.calendar.entity.ScheduleType;
 import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.NotNull;
 import java.time.LocalDate;
 import java.time.LocalTime;
 import lombok.Builder;
 
 public record CalendarScheduleRequest(
-    @NotEmpty(message = "스케줄 시작일은 필수 입력 값입니다.")
+    @NotNull(message = "스케줄 시작일은 필수 입력 값입니다.")
     LocalDate dateFrom,
 
-    @NotEmpty(message = "스케줄 종료일은 필수 입력 값입니다.")
+    @NotNull(message = "스케줄 종료일은 필수 입력 값입니다.")
     LocalDate dateTo,
 
     LocalTime timeFrom,
