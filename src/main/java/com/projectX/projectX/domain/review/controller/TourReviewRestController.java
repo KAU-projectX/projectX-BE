45ab72b0b6a6f8812199a0105755811fc014f073
jun23314 <dlwnsfml@naver.com>
2024-09-05 15:17:39 +0900

    valid: 리뷰 등록 컨트롤러 파라미터에 제약사항 추가

--- src/main/java/com/projectX/projectX/domain/review/controller/TourReviewRestController.java
+++ src/main/java/com/projectX/projectX/domain/review/controller/TourReviewRestController.java
@@ -9,11 +9,15 @@ import com.projectX.projectX.global.security.dto.CustomOAuth2User;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
+import jakarta.validation.constraints.Max;
+import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
 import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -25,6 +29,7 @@ import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 
+@Validated
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("v1/travels/{travel_id}/reviews")
@@ -39,8 +44,8 @@ public class TourReviewRestController {
     public ResponseDTO<String> createTourReview(
         @PathVariable("travel_id") @NotNull Long travelId,
         @RequestParam(value = "multipartFile", required = false) List<MultipartFile> files,
-        @RequestParam Integer score,
-        @RequestParam String contents,
+        @RequestParam @Min(0) @Max(5) Integer score,
+        @RequestParam @Size(min = 10, max = 500) String contents,
         @RequestParam RecommendationType recommendationType,
         @AuthenticationPrincipal CustomOAuth2User user
     ) {
