dd602b17ff13b41f91164d7ac3c19ddf633b9bd1
Yurim Yang <yang980329@likelion.org>
2024-09-11 23:13:04 +0900

    feat: 캘린더 전체 정보 조회 컨트롤러 생성

--- src/main/java/com/projectX/projectX/domain/calendar/controller/CalendarRestController.java
+++ src/main/java/com/projectX/projectX/domain/calendar/controller/CalendarRestController.java
@@ -1,15 +1,19 @@
 package com.projectX.projectX.domain.calendar.controller;
 
 import com.projectX.projectX.domain.calendar.dto.request.CalendarScheduleRequest;
+import com.projectX.projectX.domain.calendar.dto.response.AllCalendarScheduleResponse;
 import com.projectX.projectX.domain.calendar.service.CalendarService;
 import com.projectX.projectX.global.common.ResponseDTO;
 import com.projectX.projectX.global.security.dto.CustomOAuth2User;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
+import jakarta.validation.constraints.NotNull;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -33,5 +37,11 @@ public class CalendarRestController {
         return ResponseDTO.res("캘린더 스케줄링 정보 저장에 성공했습니다.");
     }
 
-
+    @GetMapping("/schedules/all/{year}")
+    @ResponseStatus(HttpStatus.OK)
+    @Operation(summary = "캘린더 전체 정보 조회 API", description = "캘린더의 전체 스케줄링 정보를 조회하는 api 입니다.")
+    public ResponseDTO<AllCalendarScheduleResponse> getAllCalendarSchedule(@PathVariable("year") int year,  @AuthenticationPrincipal CustomOAuth2User user) {
+        calendarService.getAllSchedule(year, user.getEmail());
+        return ResponseDTO.res("캘린더 스케줄링 정보 조회에 성공했습니다.");
+    }
 }
