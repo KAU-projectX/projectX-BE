28438ca103dec35d81339bdc59e2c55ea98e3380
Yurim Yang <yang980329@likelion.org>
2024-09-11 23:13:35 +0900

    feat: 캘린더 전체 정보 조회 쿼리 생성

--- src/main/java/com/projectX/projectX/domain/calendar/repository/CalendarRepository.java
+++ src/main/java/com/projectX/projectX/domain/calendar/repository/CalendarRepository.java
@@ -1,9 +1,20 @@
 package com.projectX.projectX.domain.calendar.repository;
 
+import com.projectX.projectX.domain.calendar.dto.response.AllCalendarScheduleResponse;
 import com.projectX.projectX.domain.calendar.entity.Calendar;
+import java.util.HashMap;
+import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 public interface CalendarRepository extends JpaRepository<Calendar, Long> {
+
+    @Query("""
+    SELECT c.dateFrom, c.dateTo, c.title, c.scheduleType
+    FROM Calendar c
+    WHERE c.user.id = :memberId AND FUNCTION('YEAR', c.dateFrom) = :year
+""")
+    List<AllCalendarScheduleResponse> getScheduleFromYear(int year, long memberId);
 
 
 }
